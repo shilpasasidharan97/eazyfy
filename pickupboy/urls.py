495556259a01b9bd16b435df790d3705c04fe397
viveknhc <viveknhc@gmail.com>
2022-10-12 15:10:53 +0530

    template started

--- pickupboy/urls.py
+++ pickupboy/urls.py
@@ -3,4 +3,14 @@ from . import views
 
 app_name = 'pickupboy'
 
-urlpatterns = []
+urlpatterns = [
+    path('',views.index,name="index"),
+    path('total-order',views.total_order,name="total-order"),
+    path('product-validation',views.product_validation,name="product-validation"),
+    path('product-details',views.product_details,name="product-details"),
+
+    ]
+
+
+
+    
--- pickupboy/views.py
+++ pickupboy/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+def index(request):
+    return render(request,"pickup-boy/index.html")
+
+def total_order(request):
+    return render(request,"pickup-boy/total-order.html")
+
+def product_validation(request):
+    return render(request,"pickup-boy/product-validation.html")
+
+def product_details(request):
+    return render(request,"pickup-boy/product-details.html")
+
