0ec2ce04e170552ef270a3fbac62830394090a71
viveknhc <viveknhc@gmail.com>
2022-10-13 17:13:54 +0530

    working on templates

--- pickupboy/urls.py
+++ pickupboy/urls.py
@@ -5,9 +5,14 @@ app_name = 'pickupboy'
 
 urlpatterns = [
     path('',views.index,name="index"),
+    path('base',views.base,name="base"),
     path('total-order',views.total_order,name="total-order"),
-    path('product-validation',views.product_validation,name="product-validation"),
+    path('customer-selfy',views.customer_selfy,name="customer-selfy"),
     path('product-details',views.product_details,name="product-details"),
+    path('verification-questions',views.verification_questions,name="verification-questions"),
+    path('complete',views.complete,name="complete"),
+    path('fail',views.fail,name="fail"),
+    path('requote',views.requote,name="requote"),
 
     ]
 
