065727f81e49fb54fdcd2528659479d7d322dd2a
shilpasasidharan97 <shilpasasidharan97@gmail.com>
2022-10-13 09:33:43 +0530

    new static

--- official/views.py
+++ official/views.py
@@ -1,3 +1,19 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+
+def loginPage(request):
+    return render(request,'official/login.html')
+
+
+def forgotPassword(request):
+    return render(request,'official/forgot_password.html')
+
+
+def home(request):
+    return render(request,'official/home.html')
+
+
+def brand(request):
+    return render(request,'official/brand.html')
