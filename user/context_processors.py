bf336624f4b8b885a200eba9119de2bf3143f795
viveknhc <viveknhc@gmail.com>
2022-11-07 17:02:29 +0530

    qwerty

--- user/context_processors.py
+++ user/context_processors.py
@@ -3,7 +3,10 @@ from official.models import *
 
 def main_context(request):
     brand = Brand.objects.all()
+    user_object = request.user
+   
     return {
         "domain": request.META["HTTP_HOST"],
         "brand":brand,
+        "user_object" : user_object,
     }
