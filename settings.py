2088fb3002fd8694743a7df0cc2a21a535281704
shilpasasidharan97 <shilpasasidharan97@gmail.com>
2022-11-10 11:27:42 +0530

    transactions

--- settings.py
+++ settings.py
@@ -125,13 +125,15 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/4.1/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = "en-us"
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = "Asia/Kolkata"
 
 USE_I18N = True
 
-USE_TZ = True
+USE_L10N = True
+
+USE_TZ = True 
 
 
 # Static files (CSS, JavaScript, Images)
