2dc2d611fa71717cb8c0526de0677bd8e698896f
Shahid <mhdshd.ak@gmail.com>
2022-12-29 16:50:06 +0530

    dfgsdf

--- user/views.py
+++ user/views.py
@@ -247,7 +247,15 @@ def question(request,id):
         "questions":objective_questions,
         "image_questions":image_questions,
     }
-    return render(request,"user/question.html",context)  
+    return render(request,"user/question.html",context)
+
+@csrf_exempt
+def save_answer(request):
+    data = json.loads(request.POST['data'])
+    yes_no_ans = data[0]
+    print(yes_no_ans)
+    print(len(yes_no_ans))
+    return JsonResponse({'msg':"Success"})
 
 
 # MODEL SPECIFICATIONS
